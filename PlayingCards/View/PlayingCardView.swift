51758a228d6f6cd7285867dcdd60e4b2f8d0c75d
Pavan Kumar N <pavankumar.n.46@gmail.com>
2019-11-02 13:04:51 +0530

    Lecture 6: Example of using UIGraphicsGetCurrentContext() and using UIBezierPath()

---
+++ PlayingCards/View/PlayingCardView.swift
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.swift
+//  PlayingCards
+//
+//  Created by Pavan Kumar N on 02/11/19.
+//  Copyright © 2019 https://github.com/pavankumar-n-46 . All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+//        if let contextPath = UIGraphicsGetCurrentContext() {
+//            contextPath.addArc(center: CGPoint(x: bounds.midX,y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            contextPath.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            contextPath.fillPath()
+//        }
+        
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX,y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.lineWidth = 5.0
+//        UIColor.green.setFill()
+//        UIColor.red.setStroke()
+//        path.fill()
+//        path.stroke()
+
+    }
+}
